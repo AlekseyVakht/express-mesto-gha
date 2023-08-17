5875dcbbb3ac07ec1374b644a22e5417c23e1015
Aleksey Vakht <AlekseyVakht@yandex.ru>
2023-08-17 14:18:01 +0300

    ver 1.4

--- controllers/user.js
+++ controllers/user.js
@@ -1,15 +1,14 @@
-const mongoose = require('mongoose');
 const User = require('../models/user');
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar }, { new: true, runValidators: true })
     .then((user) => res.send({ data: user }))
-    .catch((err) => {
-      if (err instanceof mongoose.Error.CastError) {
-        res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя' });
-      } else {
-        res.status(500).send({ message: 'Ошибка на сервере' });
+    .catch(() => {
+      if (res.status(400)) {
+        res.send({ message: 'Переданы некорректные данные при создании пользователя' });
+      } else if (res.status(500)) {
+        res.send({ message: 'Ошибка на сервере' });
       }
     });
 };
@@ -17,11 +16,11 @@ module.exports.createUser = (req, res) => {
 module.exports.getUserById = (req, res) => {
   User.findById(req.params.userId)
     .then((user) => res.send({ data: user }))
-    .catch((err) => {
-      if (err instanceof mongoose.Error.DocumentNotFoundError) {
-        res.status(404).send({ message: 'Пользователь с указанным Id не найден' });
-      } else {
-        res.status(500).send({ message: 'Ошибка на сервере' });
+    .catch(() => {
+      if (res.status(404)) {
+        res.send({ message: 'Пользователь с указанным Id не найден' });
+      } else if (res.status(500)) {
+        res.send({ message: 'Ошибка на сервере' });
       }
     });
 };
@@ -36,11 +35,11 @@ module.exports.updateUser = (req, res) => {
   const { name, about } = req.body;
   User.findByIdAndUpdate(req.user._id, { name, about })
     .then((user) => res.send({ data: user }))
-    .catch((err) => {
-      if (err instanceof mongoose.Error.DocumentNotFoundError) {
-        res.status(404).send({ message: 'Пользователь с указанным Id не найден' });
-      } else {
-        res.status(500).send({ message: 'Ошибка на сервере' });
+    .catch(() => {
+      if (res.status(404)) {
+        res.send({ message: 'Пользователь с указанным Id не найден' });
+      } else if (res.status(500)) {
+        res.send({ message: 'Ошибка на сервере' });
       }
     });
 };
@@ -49,11 +48,11 @@ module.exports.updateUserAvatar = (req, res) => {
   const { avatar } = req.body;
   User.findByIdAndUpdate(req.user._id, { avatar })
     .then((users) => res.send({ data: users }))
-    .catch((err) => {
-      if (err instanceof mongoose.Error.DocumentNotFoundError) {
-        res.status(404).send({ message: 'Пользователь с указанным Id не найден' });
-      } else {
-        res.status(500).send({ message: 'Ошибка на сервере' });
+    .catch(() => {
+      if (res.status(404)) {
+        res.send({ message: 'Пользователь с указанным Id не найден' });
+      } else if (res.status(500)) {
+        res.send({ message: 'Ошибка на сервере' });
       }
     });
 };
