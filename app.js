fe14afad59c69fc570ba7520ffc53685e792982c
Aleksey Vakht <AlekseyVakht@yandex.ru>
2023-08-25 05:51:47 +0300

    ver 1.6

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.post('/signup', celebrate({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
     avatar: Joi.string(),
-    email: Joi.string().required().unique().email(),
+    email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   }).unknown(true),
 }), createUser);
