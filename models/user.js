6b756bb7be306f9da414b71ecc93932761b97e17
Aleksey Vakht <AlekseyVakht@yandex.ru>
2023-08-25 05:57:02 +0300

    ver 1.7

--- models/user.js
+++ models/user.js
@@ -18,13 +18,7 @@ const userSchema = new mongoose.Schema({
   },
   avatar: {
     type: String,
-    default: 'ссылка',
-    validate: {
-      validator(v) {
-        return /^https?:\/\/(?:www\.)?[-a-zA-Z0-9!-._~:/?#[]@!&'()+,;=]{1,256}\.[-a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()!-._~:/?#[]@!&'()+,;=]*)$/.test(v);
-      },
-      message: 'Invalid path',
-    },
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
   email: {
     type: String,
