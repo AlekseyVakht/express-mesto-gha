8d901fe6a0a7084429198f1e7c0acee6ed2efd77
Aleksey Vakht <AlekseyVakht@yandex.ru>
2023-08-27 02:00:40 +0300

    ver 1.3.9

--- models/user.js
+++ models/user.js
@@ -18,7 +18,7 @@ const userSchema = new mongoose.Schema({
   },
   avatar: {
     type: String,
-    default: 'http://ya-ya-ya.ru',
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
   email: {
     type: String,
