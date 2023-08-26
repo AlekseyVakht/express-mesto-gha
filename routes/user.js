2cd7b9904a423484530c38c2f6374fea8b944b59
Aleksey Vakht <AlekseyVakht@yandex.ru>
2023-08-27 01:19:59 +0300

    ver 1.2.7

--- routes/user.js
+++ routes/user.js
@@ -13,7 +13,7 @@ router.get('/me', getCurrentUser);
 
 router.get('/:userId', celebrate({
   params: Joi.object().keys({
-    userId: Joi.string().alphanum(),
+    userId: Joi.string().alphanum().length(24),
   }).unknown(true),
 }), getUserById);
 
